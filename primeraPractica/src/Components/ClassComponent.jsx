79e605141457e1cb9695d0a3d2a36d5baf6fc710
samirabarrera <samira.barrera@galileo.edu>
2025-02-27 23:10:55 -0600

    first commit

---
+++ primeraPractica/src/Components/ClassComponent.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ClassComponent extends Component {
+    render() {
+        return <h2>Hola, soy un componente de clase</h2>;
+    }
+}
+
+export default ClassComponent
---
+++ primeraPractica/src/Components/FunctionComponent.jsx
@@ -0,0 +1,5 @@
+const FunctionComponent = () => {
+    return <h2>Hola, soy un componente funcional</h2>
+};
+
+export default FunctionComponent;
