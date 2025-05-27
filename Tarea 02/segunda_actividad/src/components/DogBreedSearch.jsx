69a8f0b0858b392525849acb4639c90b409f3927
Haroldo José López de los Ríos <haroldo.lopez@atomchat.io>
2025-05-27 09:06:35 -0600

    Adding the 10 requests to the dog API

--- Tarea 02/segunda_actividad/src/components/DogBreedSearch.jsx
+++ Tarea 02/segunda_actividad/src/components/DogBreedSearch.jsx
@@ -1,25 +1,32 @@
 import { useEffect, useState } from "react";
 
 export default function DogBreedSearch () {
-    const [dogList, setDogList] = useState([]);
-    const getData = async () => {
-        try {
-            const imgURL = await Promise.all(Array.from({length:10})
-            .map(() => fetch('https://dog.ceo/api/breeds/image/random')
-                .then(res = res.json())
-                .then(data => data.message)))
-            setDogList(imgURL);
-        } catch (errMsg) {
-            console.error("ERror al obtener listado de perros: ", errMsg);
-        }
+
+  const [dogList, setDogList] = useState([]);
+
+
+  const getData = async () => {
+    try {
+      // Se puede utilizar un Promise.all para obtener a partir de un array de 10 elemenos hacer un
+      // fetch por cada elmento obteniendo la respuesta de cada uno
+      const imgUrls = await Promise.all(Array.from({length:10})
+                        .map(()=> fetch('https://dog.ceo/api/breeds/image/random')
+                                .then(res => res.json())
+                                .then(data => data.message)))
+      setDogList(imgUrls); // Se guarda la respuesa de cada uno en un arreglo
+    }catch(errMsg) {
+      console.error("Error al obtener listado de perros: ", errMsg);
     }
-    useEffect(() => {
-        getData()
-    },[])
+  }
 
-    return(
+  // Colocar el useEffect para que se ejecute una vez al crear el componente
+  useEffect(()=>{
+    getData()
+  },[])
+
+    return (
         <div>
-
+          
         </div>
     )
 };
