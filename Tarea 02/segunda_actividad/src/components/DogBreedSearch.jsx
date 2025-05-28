5c3875b492cc8b517579e5cfc37f9849cb665f9e
samirabarrera <samira.barrera@galileo.edu>
2025-05-27 19:17:18 -0600

    adding segunda actividad

--- Tarea 02/segunda_actividad/src/components/DogBreedSearch.jsx
+++ Tarea 02/segunda_actividad/src/components/DogBreedSearch.jsx
@@ -1,14 +1,25 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function DogBreedSearch () {
-    return (
+    const [dogList, setDogList] = useState([]);
+    const getData = async () => {
+        try {
+            const imgURL = await Promise.all(Array.from({length:10})
+            .map(() => fetch('https://dog.ceo/api/breeds/image/random')
+                .then(res = res.json())
+                .then(data => data.message)))
+            setDogList(imgURL);
+        } catch (errMsg) {
+            console.error("ERror al obtener listado de perros: ", errMsg);
+        }
+    }
+    useEffect(() => {
+        getData()
+    },[])
+
+    return(
         <div>
-           
+
         </div>
     )
 };
-
-/*{
-"message": "https: \/\/images.dog.ceo\/breeds\/samoyed\/n02111889_2789.jpg",
-"status": "success",
-}*/
