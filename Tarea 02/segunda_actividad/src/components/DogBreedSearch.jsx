f28cd0f14996468c26308f4a4402f3c8c77fcd38
samirabarrera <samira.barrera@galileo.edu>
2025-05-27 19:17:18 -0600

    adding segunda actividad

--- Tarea 02/segunda_actividad/src/components/DogBreedSearch.jsx
+++ Tarea 02/segunda_actividad/src/components/DogBreedSearch.jsx
@@ -30,8 +30,3 @@ export default function DogBreedSearch () {
         </div>
     )
 };
-
-/*{
-"message": "https: \/\/images.dog.ceo\/breeds\/samoyed\/n02111889_2789.jpg",
-"status": "success",
-}*/
