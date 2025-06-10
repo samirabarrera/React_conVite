6bdc15e6fd7f20a33f4d4bece513b5635fe43761
Haroldo José López de los Ríos <haroldo.lopez@atomchat.io>
2025-06-10 12:03:34 -0600

    Add change to product component

--- Tarea 04/coffee-shop/src/hooks/useFetch.js
+++ Tarea 04/coffee-shop/src/hooks/useFetch.js
@@ -5,6 +5,8 @@ export default function useFetch(url) {
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null)
 
+    const [products, setProducts] = useState([])
+
     useEffect(() => {
         fetch(url)
             .then(res => {
@@ -12,8 +14,9 @@ export default function useFetch(url) {
                 return res.json()
             })
             .then(data => {
-                setData(data)
-                setLoading(false)
+              const priceAleatorio = Math.floor(Math.random() * (300 - 100 + 1)) + 100;
+              setData(data.map(d => ({...d, price:priceAleatorio, cantidad: 1})))
+              setLoading(false)
             })
             .catch(err => {
                 setError(err.message)
@@ -25,5 +28,5 @@ export default function useFetch(url) {
         console.log("Datos obtenidos desde useFetch.")
     }
 
-    return { data, loading, error, mensaje }
+    return { data, loading, error, mensaje, products, setProducts }
 }
